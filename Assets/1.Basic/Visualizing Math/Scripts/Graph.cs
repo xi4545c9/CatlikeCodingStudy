fbe1665a23518fb0f13792b56e5b19212ec7e234
dogfish <xi4545c9@163.com>
2021-08-21 21:56:18 +0800

    Visualizing Math

---
+++ Assets/1.Basic/Visualizing Math/Scripts/Graph.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+  [SerializeField]
+  private Transform pointPrefab = default;
+
+  [SerializeField,Range(10,100)] 
+  private int resolution = 10;
+  
+  
+  private void Awake()
+  {
+    float step = 2f / resolution;
+    var position = Vector3.zero;
+    var scale = Vector3.one*step;
+
+    
+    for (int i = 0; i < resolution; i++)
+    {
+      Transform point=Instantiate(pointPrefab);
+      position.x = (i + 0.5f) *step - 1f;
+      position.y = position.x*position.x*position.x;
+      point.localPosition = position;
+      point.localScale = scale;
+      point.SetParent(transform,false);
+    }
+   
+    
+  }
+}
