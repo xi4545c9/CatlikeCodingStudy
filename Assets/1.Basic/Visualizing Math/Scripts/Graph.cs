dccd69326d3dd5a4d03fc60efbb92db06f4e0a4e
dogfish <xi4545c9@163.com>
2021-08-22 13:35:50 +0800

    Visualizing Math URP

--- Assets/1.Basic/Visualizing Math/Scripts/Graph.cs
+++ Assets/1.Basic/Visualizing Math/Scripts/Graph.cs
@@ -8,7 +8,8 @@ public class Graph : MonoBehaviour
 
   [SerializeField,Range(10,100)] 
   private int resolution = 10;
-  
+
+  private Transform[] points;
   
   private void Awake()
   {
@@ -16,17 +17,27 @@ public class Graph : MonoBehaviour
     var position = Vector3.zero;
     var scale = Vector3.one*step;
 
-    
+    points = new Transform[resolution];
     for (int i = 0; i < resolution; i++)
     {
       Transform point=Instantiate(pointPrefab);
       position.x = (i + 0.5f) *step - 1f;
-      position.y = position.x*position.x*position.x;
+      //position.y = position.x*position.x*position.x;
       point.localPosition = position;
       point.localScale = scale;
       point.SetParent(transform,false);
+      points[i] = point;
     }
-   
-    
+  }
+  private void Update()
+  {
+    float time = Time.time;
+    for (int i = 0; i < points.Length; i++)
+    {
+      Transform point = points[i];
+      Vector3 position = point.localPosition;
+      position.y = Mathf.Sin(Mathf.PI*(position.x+time));
+      point.localPosition = position;
+    }
   }
 }
