201d2b9ca4b3d9f7b22ff4aec08b642428601d8e
dogfish <xi4545c9@163.com>
2021-08-23 13:06:56 +0800

    球前面鸽了

--- Assets/1.Basic/3.Sculpting with Numbers/Scripts/FunctionLibrary.cs
+++ Assets/1.Basic/3.Sculpting with Numbers/Scripts/FunctionLibrary.cs
@@ -4,24 +4,52 @@ public static class FunctionLibrary
 {
   public delegate float Function(float x, float t);
 
+  public delegate Vector3 Function3D(float u, float v, float t);
+
   public enum FunctionName
   {
     Wave,MultiWave,Ripple
   }
   private static Function[] functions={Wave,MultiWave,Ripple};
+  private static Function3D[] function3Ds = { Wave, MultiWave, Ripple };
   public static Function GetFunction(FunctionName name)
   {
     return functions[(int)name];
+  }
+
+  public static Function3D GetFunction3D(FunctionName name)
+  {
+    return function3Ds[(int)name];
   }
   public static float Wave(float x,float t)
   {
     return Sin(PI * (x + t));
   }
+
+  public static Vector3 Wave(float u, float v, float t)
+  {
+    Vector3 p;
+    p.x = u;
+    p.y = Sin(PI * (u + v + t));
+    p.z = v;
+    return p;
+  }
   public static float MultiWave(float x,float t)
   {
     float y = Sin(PI * x + 0.5f*t);
     y += 0.5f*Sin(2f * PI * (x + t));
+    
     return y*(2f/3f);
+  }
+  public static Vector3 MultiWave(float u,float v,float t)
+  {
+    Vector3 p;
+    p.x = u;
+    p.y = Sin(PI * (u + 0.5f * t));
+    p.y += 0.5f * Sin(2f * PI * (v + t));
+    p.y *= 1f / 2.5f;
+    p.z = v;
+    return p;
   }
 
   public static float Ripple(float x, float t)
@@ -29,5 +57,15 @@ public static class FunctionLibrary
     float d = Abs(x);
     float y = Sin(PI*(4f*d-t));
     return y/(1f+10f*d);
+  }
+  public static Vector3 Ripple(float u,float v, float t)
+  {
+    float d = Sqrt(u * u + v * v);
+    Vector3 p;
+    p.x = u;
+    p.y = Sin(PI * (4f * d - t));
+    p.y /= 1f + 10f * d;
+    p.z = v;
+    return p;
   }
 }
---
+++ Assets/1.Basic/3.Sculpting with Numbers/Scripts/Graph3D.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Graph3D : MonoBehaviour
+{
+    [SerializeField]
+    private Transform pointPrefab = default;
+
+    [SerializeField,Range(10,100)] 
+    private int resolution = 10;
+
+    //[SerializeField, Range(0, 2)] 
+    //private int function = 0;
+    [SerializeField] 
+    private FunctionLibrary.FunctionName function = default;
+  
+    private Transform[] points;
+  
+    private void Awake()
+    {
+        float step = 2f / resolution;
+        //var position = Vector3.zero;
+        var scale = Vector3.one*step;
+        points = new Transform[resolution*resolution];
+        for (int i = 0,x=0,z=0; i < points.Length; i++,x++)
+        {
+            /*if (x == resolution)
+            {
+                x = 0;
+                z++;
+            }*/
+            Transform point=Instantiate(pointPrefab);
+            /*position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
+            point.localPosition = position;*/
+            point.localScale = scale;
+            point.SetParent(transform,false);
+            points[i] = point;
+        }
+    }
+    private void Update()
+    {
+        FunctionLibrary.Function3D f = FunctionLibrary.GetFunction3D(function);
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0,x=0,z=0; i < points.Length; i++,x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z++;
+                v = (z + 0.5f) * step - 1f;
+            }
+
+            float u = (x + 0.5f) * step - 1f;
+            //float v = (z + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
+        }
+    }
+}
